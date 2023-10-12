6052f637ec2f709d9c51fa0f207c42860f5b6a1d
김수민 <rtn2001@naver.com>
2023-10-12 17:17:26 +0900

    인사 정보 SPA 구축 Add PersonForm component
    - SignupPage에 PersonForm compoment 추가

--- 인사 정보 SPA 구축/src/components/PersonForm.js
+++ 인사 정보 SPA 구축/src/components/PersonForm.js
@@ -6,47 +6,73 @@ export default class PersonForm {
   }
 
   template() {
+    // add span tag function
+    const addSpanTag = (field) => {
+      const $span = document.createElement("span");
+      $span.classList.add("form_elem");
+      if (field) {
+        $span.innerHTML = `
+          <label>${field.text}</label>
+          <span class="mark">(필수*)</span>
+        `; 
+      } else {
+        $span.innerHTML = `
+          <button type="submit">등록</button>
+        `; 
+      }
+      return $span;
+    };
+    
     const $div = document.createElement("div");
     $div.id = "form_container";
-    $div.innerHTML = `        
-      <span class="form_elem">
-        <input id="name" placeholder="이름">
-      </span>
-      <span class="form_elem">
-        <input id="email" placeholder="이메일">
-      </span>
-      <span class="form_elem">
-        <input id="nickname" placeholder="닉네임">
-      </span>
-      <span class="form_elem">
-        <select id="role" name="role">
-          <option value="">직군을 선택해주세요</option>
-          <option value="backend">백엔드</option>
-          <option value="frontend">프론트엔드</option>
-          <option value="fullstack">풀스택</option>
-        </select>
-      </span>
-      <span class="form_elem">
-        <select id="mbti" name="mbti">
-          <option value="">MBTI를 선택해주세요</option>
-          <option value="backend">mbti1</option>
-          <option value="frontend">mbti2</option>
-          <option value="fullstack">mbti3</option>
-        </select>
-      </span>
-      <span class="form_elem">
-        <button type="submit">등록</button>
-      </span>`;
+    const fieldMap = this.state.fieldMap;
+    // input tag
+    const inputChild = fieldMap.input;
+    inputChild.map(field => {
+      const $span = addSpanTag(field);
+      const $input = document.createElement("input");
+      $input.id = `${field.id}`;
+      $input.placeholder = `${field.text}`;
+      $span.appendChild($input);
+      $div.appendChild($span);
+    });
+    
+    // select tag
+    const selectChild = fieldMap.select;
+    selectChild.map(field => {
+      const $span = addSpanTag(field);
+      const $select = document.createElement("select");
+      $select.id = $select.name = `${field.id}`;
+      // select option
+      const optionList = (field.id==="job")? this.state.jobList : this.state.mbtiList;
+      $select.innerHTML = `
+        ${optionList.map((option) => `
+        <option value="${option.value}">${option.text}</option>
+        `).join("")}
+      `;
+      $span.appendChild($select);
+      $div.appendChild($span);
+    });
 
+    // button
+    const $span = addSpanTag();
+    $div.appendChild($span);
     return $div;
   }
 
-      
+  
   render() {
     this.$target.appendChild(this.template());
   }
 
   setState() {
+    // component를 그리기 위한 state setting
+    const fieldMap = {"input": [{"text": "이름", "id" :"name"}, {"text": "이메일", "id" :"email"}, {"text": "닉네임", "id" :"nickname"}], "select": [{"text": "직군", "id" :"job"}, {"text": "MBTI", "id": "mbti"}]};
+    const jobList = [{"text": "직군을 선택해주세요", "value": ""}, {"text": "백엔드", "value": "backend"}, {"text": "프론트엔드", "value": "frontend"}, {"text": "풀스택", "value": "fullstack"}];
+    const mbtiList = [{"text":"MBTI를 선택해주세요", "value": ""}, {"text":"ENFJ", "value":"ENFJ"}, {"text":"ENTJ", "value":"ENTJ"}, {"text":"ENFP", "value":"ENFP"}, {"text":"ENTP", "value":"ENTP"}, {"text":"ESFJ", "value":"ESFJ"}, {"text":"ESTJ", "value":"ESTJ"}, {"text":"ESFP", "value":"ESFP"}, {"text":"ESTP", "value":"ESTP"}, {"text":"INFJ", "value":"INFJ"}, {"text":"INTJ", "value":"INTJ"}, {"text":"INFP", "value":"INFP"}, {"text":"INTP", "value":"INTP"}, {"text":"ISFJ", "value":"ISFJ"}, {"text":"ISTP", "value":"ISTP"}];
+    this.state = {...this.state, ...{ "fieldMap": fieldMap }, ...{ "jobList": jobList }, ...{ "mbtiList": mbtiList }};
     this.render();
   }
+
+  setVi
 }
